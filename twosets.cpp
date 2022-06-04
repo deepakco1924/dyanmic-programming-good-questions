8c14d544a68924a4a02db02cf0be08bb21fa4b96
Deepak Pal <co19524@ccet.ac.in>
2022-06-04 17:59:41 +0530

    added solution of two setII

---
+++ twosets.cpp
@@ -0,0 +1,142 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+ 
+using namespace std;
+using namespace chrono;
+using namespace __gnu_pbds;
+ 
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define triple pair<pair<int,int>,int>
+#define MOD 1000000007
+#define MOD1 998244353
+#define clr(x, y) memset(x, y, sizeof(x))
+#define pii pair<int, int>
+#define vi vector<int>
+#define pqb priority_queue<int>
+#define pqs priority_queue<int, vi, greater<int>>
+#define node_pair pair<pair<int,int>,int>
+#define INF 1e18
+#define MINF LLONG_MIN
+#define endl "\n"
+#define pqb priority_queue<int>
+#define pqs priority_queue<int, vi, greater<int>>
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define int long long
+#define w(x) \
+  int x;     \
+  cin >> x;  \
+  while (x--)
+ 
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+ 
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+template <typename T>
+using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+// void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+ 
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T>
+void _print(pbds<T> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+ 
+/*---------------------------------------------------------------------------------------------------------------------------*/
+ll gcd(ll a, ll b) {if (b > a) {return gcd(b, a);} if (b == 0) {return a;} return gcd(b, a % b);}
+ll expo(ll a, ll b, ll mod) {ll res = 1; while (b > 0) {if (b & 1)res = (res * a) % mod; a = (a * a) % mod; b = b >> 1;} return res;}
+void extendgcd(ll a, ll b, ll*v) {if (b == 0) {v[0] = 1; v[1] = 0; v[2] = a; return ;} extendgcd(b, a % b, v); ll x = v[1]; v[1] = v[0] - v[1] * (a / b); v[0] = x; return;} //pass an arry of size1 3
+ll mminv(ll a, ll b) {ll arr[3]; extendgcd(a, b, arr); return arr[0];} //for non prime b
+ll mminvprime(ll a, ll b) {return expo(a, b - 2, b);}
+bool revsort(ll a, ll b) {return a > b;}
+void swap(int &x, int &y) {int temp = x; x = y; y = temp;}
+ll combination(ll n, ll r, ll m, ll *fact, ll *ifact) {ll val1 = fact[n]; ll val2 = ifact[n - r]; ll val3 = ifact[r]; return (((val1 * val2) % m) * val3) % m;}
+void google(int t) {cout << "Case #" << t << ": ";}
+vector<ll> sieve(int n) {int*arr = new int[n + 1](); vector<ll> vect; for (int i = 2; i <= n; i++)if (arr[i] == 0) {vect.push_back(i); for (int j = 2 * i; j <= n; j += i)arr[j] = 1;} return vect;}
+ll mod_add(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a + b) % m) + m) % m;}
+ll mod_mul(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a * b) % m) + m) % m;}
+ll mod_sub(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a - b) % m) + m) % m;}
+ll mod_div(ll a, ll b, ll m) {a = a % m; b = b % m; return (mod_mul(a, mminvprime(b, m), m) + m) % m;}  //only for prime m
+ll phin(ll n) {ll number = n; if (n % 2 == 0) {number /= 2; while (n % 2 == 0) n /= 2;} for (ll i = 3; i <= sqrt(n); i += 2) {if (n % i == 0) {while (n % i == 0)n /= i; number = (number / i * (i - 1));}} if (n > 1)number = (number / n * (n - 1)) ; return number;} //O(sqrt(N))
+/*--------------------------------------------------------------------------------------------------------------------------*/
+
+void solve(){
+int n;
+cin>>n;
+int sum=(n*(n+1LL))/2LL;
+if(sum%2!=0){
+  cout<<0<<endl;
+  return ;
+}
+sum=sum/2LL;
+int dp[n+1][sum+1];
+dp[0][0]=1;
+for(int i=1;i<=sum;i++){
+ dp[0][i]=0;
+}
+for(int i=1;i<=n;i++){
+ for(int j=0;j<=sum;j++){
+  if(j==0){
+    dp[i][j]=1;
+  }
+  else{
+    //here we arrive to cases what whould be the answer so 
+    int op1=dp[i-1][j];
+    int op2=(j>=i)?dp[i-1][j-i]:0LL;
+    dp[i][j]=(op1+op2)%MOD;
+  }
+ }
+
+}
+int ans=dp[n][sum];
+int val=mminv(2LL,MOD);
+ans=mod_mul(ans,val,MOD);
+cout<<ans<<endl;
+
+
+
+}
+int32_t main() {
+ 
+#ifndef ONLINE_JUDGE
+  freopen("error.txt", "w", stderr);
+#endif
+ 
+  fastio();
+  auto start1 = high_resolution_clock::now();
+  int t = 1;
+  // cin >> t;
+  while (t--)solve();
+  auto stop1 = high_resolution_clock::now();
+  auto duration = duration_cast<microseconds>(stop1 - start1);
+#ifndef ONLINE_JUDGE
+  cerr << "Time: " << duration . count() / 1000 << endl;
+#endif
+  return 0;
+}
